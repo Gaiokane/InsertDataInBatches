95ed93d8cce2e837351d2ada48add6381c7476d3
Gaiokane <qk632773809@vip.qq.com>
2020-07-13 23:17:47 +0800

    优化：分别记录MSSQL/MySQL上次连接记录（完成切换数据库类型自动读取上次连接记录）

--- InsertDataInBatches/FrmMain.cs
+++ InsertDataInBatches/FrmMain.cs
@@ -30,7 +30,7 @@ namespace InsertDataInBatches
          * 2.增加连接记录（数据库地址、数据库等）维护页面
          * 3.若配置文件中 快捷插入配置/常用SQL value为“;”，程序运行后下拉框会为空（当前使用；代替;快捷插入配置：在指定元素中随机选择一项）
          * 4.多线程插数据
-         * 5.连接记录分别记录mssql、mysql最近一次连接记录——读取（√）、连接成功更新配置（）、无配置恢复默认（√）、切换数据库类型自动读取上次连接记录
+         * 5.连接记录分别记录mssql、mysql最近一次连接记录——读取（√）、连接成功更新配置（√）、无配置恢复默认（√）、切换数据库类型自动读取上次连接记录（√）
          */
 
         /* 
@@ -809,7 +809,7 @@ namespace InsertDataInBatches
         #region 数据库_单选按钮切换默认端口改变
         private void radiobtnMSSQL_CheckedChanged(object sender, EventArgs e)
         {
-            if (radiobtnMSSQL.Checked == true)
+            /*if (radiobtnMSSQL.Checked == true)
             {
                 txtboxPort.Text = "1433";
 
@@ -830,6 +830,88 @@ namespace InsertDataInBatches
                 txtboxUsername.Text = "qkk";
                 txtboxPassword.Text = "qkk";
                 richtxtboxInsertSQL.Text = "INSERT INTO `pagination`.`info`(`xxx`) VALUES ('test{{id:7}}');";
+            }*/
+
+
+            int isLastConnectionSettingError = 0;
+            string[] LastConnectionStrings;
+            string radiobtn;
+            if (radiobtnMSSQL.Checked == true)
+            {
+                radiobtn = "MSSQL";
+            }
+            else
+            {
+                radiobtn = "MySQL";
+            }
+
+            #region 判断MSSQL的LastConnectionStrings、MySQL的LastConnectionStrings是否有问题，对有问题部分重置为默认值
+            LastConnectionStrings = ConfigSettings.getLastConnectionStrings(radiobtn);
+            if (radiobtn == "MSSQL" && LastConnectionStrings.Length != 7)
+            {
+                //MSSQL的LastConnectionStrings问题，值为2
+                isLastConnectionSettingError += 2;
+            }
+            if (radiobtn == "MySQL" && LastConnectionStrings.Length != 7)
+            {
+                //MySQL的LastConnectionStrings问题，值为4
+                isLastConnectionSettingError += 4;
+            }
+
+            //MSSQL的LastConnectionStrings问题，值为2
+            if (getAndOperationResult(isLastConnectionSettingError, 2) != 0)
+            {
+                ConfigSettings.setLastConnectionStrings(0, "127.0.0.1", false, "1433", "qktest", "sa", "11111");
+            }
+            //MySQL的LastConnectionStrings问题，值为4
+            if (getAndOperationResult(isLastConnectionSettingError, 4) != 0)
+            {
+                ConfigSettings.setLastConnectionStrings(1, "127.0.0.1", true, "3306", "pagination", "qkk", "qkk");
+            }
+            if (isLastConnectionSettingError != 0)
+            {
+                MessageBox.Show("最新数据库连接值不正确，已重置为默认值，请重新运行该程序！");
+                Application.Exit();
+            }
+            #endregion
+
+            bool isPort = false;
+            if (LastConnectionStrings[2] == "True")
+            {
+                isPort = true;
+            }
+            else if (LastConnectionStrings[2] == "False")
+            {
+                isPort = false;
+            }
+            else
+            {
+                isPort = false;
+            }
+            //int sqlType, string Host, bool isPort, string Port, string Database, string Username, string Password
+            if (LastConnectionStrings[0] == "0")
+            {
+                radiobtnMSSQL.Checked = true;
+                txtboxHost.Text = LastConnectionStrings[1];
+                chkboxPort.Checked = isPort;
+                txtboxPort.Text = LastConnectionStrings[3];
+                txtboxDatabase.Text = LastConnectionStrings[4];
+                txtboxUsername.Text = LastConnectionStrings[5];
+                txtboxPassword.Text = LastConnectionStrings[6];
+            }
+            else if (LastConnectionStrings[0] == "1")
+            {
+                radiobtnMYSQL.Checked = true;
+                txtboxHost.Text = LastConnectionStrings[1];
+                chkboxPort.Checked = isPort;
+                txtboxPort.Text = LastConnectionStrings[3];
+                txtboxDatabase.Text = LastConnectionStrings[4];
+                txtboxUsername.Text = LastConnectionStrings[5];
+                txtboxPassword.Text = LastConnectionStrings[6];
+            }
+            else
+            {
+                setTestConn();
             }
         }
         #endregion
